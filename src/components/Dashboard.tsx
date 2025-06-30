f0bda9ac20b36b7ccb1bc4a59a01a82901bfd433
LaBarraque32 <bpierre22001@gmail.com>
2025-06-30 08:45:08 +0200

    Updated Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -204,7 +204,7 @@ const Dashboard: React.FC = () => {
               <Users className="h-8 w-8 text-blue-600" />
             </div>
             <div className="ml-4">
-            {/* <p className="text-sm font-medium text-gray-600">Adhérents actifs</p> */}
+          <p className="text-sm font-medium text-gray-600">Adhérents actifs</p>
               <p className="text-2xl font-bold text-gray-900">
                 {members.filter(m => m.isActive).length}
               </p>
